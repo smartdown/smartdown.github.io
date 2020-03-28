1686039a090340da6d023f175538870ab7be7a3b
DoctorBud <dan@quantumclay.com>
2020-03-27 22:46:11 -0700

    Initial commit

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,121 @@
+var store = [{
+        "title": "Page Not Found",
+        "excerpt":"Sorry, but the page you were trying to view does not exist --- perhaps you can try searching for it below.    ","categories": [],
+        "tags": [],
+        "url": "/404.html",
+        "teaser": null
+      },{
+        "title": "About",
+        "excerpt":"### About the Site This is a [GitHub Pages](https://pages.github.com)-based blog that utilizes the Jekyll services provided by GitHub, and optionally enables some posts and pages to utilize Smartdown, an in-browser renderer of enhanced Markdown and a reactive runtime. The source for this site is at [smartdown/blog-kit](https://github.com/smartdown/blog-kit/) (v0.0.4). ### About the...","categories": [],
+        "tags": [],
+        "url": "/pages/About/",
+        "teaser": null
+      },{
+        "title": "Dungeon Game Starter Title",
+        "excerpt":"##### Beginnings of a Dungeon Game [Character Name](:?N) ```p5js/playable/autoplay var spriteX; var spriteY; var statusHeight = 30; var controlHeight = 50; var trapezoidWidth = 100; var trapezoidHeight = 200; var upButton, downButton, leftButton, rightButton, spaceButton; p5.setup = function () { p5.createCanvas(100, 100); p5.textSize(20); p5.textFont('Helvetica'); p5.strokeWeight(2); function makeButton(label, right, bottom, keyCode)...","categories": [],
+        "tags": [],
+        "url": "/pages/DungeonGameStarter/",
+        "teaser": null
+      },{
+        "title": "Simple",
+        "excerpt":"##### Simple  The following links are inter-document links within the site, efficiently loading new Smartdown content into the currently loaded website.  - Check out the [Dungeon](:@/pages/DungeonGameStarter.md) - Check out [About](:@/pages/About.md) - Back to [Simple](:@/pages/Simple.md) ","categories": [],
+        "tags": [],
+        "url": "/pages/Simple/",
+        "teaser": null
+      },{
+        "title": "Posts by Category",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "/categories/",
+        "teaser": null
+      },{
+        "title": "Posts by Tag",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "/tags/",
+        "teaser": null
+      },{
+        "title": "Posts by Year",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "/posts/",
+        "teaser": null
+      },{
+        "title": "Welcome to My Smartdown-enabled Blog",
+        "excerpt":"## Welcome to My Smartdown-enabled Blog This is the first post in this example Blog, created with [smartdown/blog-kit](https://github.com/smartdown/blog-kit/). ### Testing Liquid-immunity via `raw` - Test of `{{ site.baseurl }}`: {{ site.baseurl }} - Test of `{{ page.path }}`: {{ page.path }} ### Tunnel Links [Smartdown Tour](:@/posts/ABriefTourOfSmartdown.md) [Welcome To My Blog](:@/posts/WelcomeToMyBlog.md)...","categories": ["smartdown"],
+        "tags": ["welcome","smartdown"],
+        "url": "/posts/WelcomeToMyBlog/",
+        "teaser": null
+      },{
+        "title": "Plain Old Markdown",
+        "excerpt":"## GitHub-Flavored Markdown with Kramdown BlogKit supports the standard [GitHub-flavored Markdown](https://help.github.com/articles/about-writing-and-formatting-on-github/) provided by GitHub Pages. Specifically, GitHub Pages uses the [kramdown](https://kramdown.gettalong.org/quickref.html) processor to convert Markdown into HTML within the GitHub Pages server. ### Markdown Examples Here are some simple examples of Markdown. #### Styles It's very easy to make some...","categories": ["markdown"],
+        "tags": ["markdown"],
+        "url": "/posts/PlainOldMarkdown/",
+        "teaser": null
+      },{
+        "title": "A Brief Tour of Smartdown",
+        "excerpt":"# WelcomeToSmartdown --- ## Welcome to Smartdown Smartdown is an extension of Markdown that includes several additions: - Technical notation support, including [MathJax](https://www.mathjax.org) LaTeX and AsciiMath syntax, chemical notation via [`mhchem`](https://mhchem.github.io/MathJax-mhchem/), - Syntax highlighting via [highlight.js](https://highlightjs.org). - Reactive cells similar to those in a spreadsheet, but inline with Smartdown prose....","categories": ["smartdown"],
+        "tags": ["welcome","smartdown"],
+        "url": "/posts/ABriefTourOfSmartdown/",
+        "teaser": null
+      },{
+        "title": "Page Not Found",
+        "excerpt":"Sorry, but the page you were trying to view does not exist --- perhaps you can try searching for it below.    ","categories": [],
+        "tags": [],
+        "url": "/404.html",
+        "teaser": null
+      },{
+        "title": "About",
+        "excerpt":"### About the Site This is a [GitHub Pages](https://pages.github.com)-based blog that utilizes the Jekyll services provided by GitHub, and optionally enables some posts and pages to utilize Smartdown, an in-browser renderer of enhanced Markdown and a reactive runtime. The source for this site is at [smartdown/blog-kit](https://github.com/smartdown/blog-kit/) (v0.0.4). ### About the...","categories": [],
+        "tags": [],
+        "url": "/pages/About",
+        "teaser": null
+      },{
+        "title": "Dungeon Game Starter Title",
+        "excerpt":"##### Beginnings of a Dungeon Game [Character Name](:?N) ```p5js/playable/autoplay var spriteX; var spriteY; var statusHeight = 30; var controlHeight = 50; var trapezoidWidth = 100; var trapezoidHeight = 200; var upButton, downButton, leftButton, rightButton, spaceButton; p5.setup = function () { p5.createCanvas(100, 100); p5.textSize(20); p5.textFont('Helvetica'); p5.strokeWeight(2); function makeButton(label, right, bottom, keyCode)...","categories": [],
+        "tags": [],
+        "url": "/pages/DungeonGameStarter",
+        "teaser": null
+      },{
+        "title": "Simple",
+        "excerpt":"##### Simple  The following links are inter-document links within the site, efficiently loading new Smartdown content into the currently loaded website.  - Check out the [Dungeon](:@/pages/DungeonGameStarter.md) - Check out [About](:@/pages/About.md) - Back to [Simple](:@/pages/Simple.md) ","categories": [],
+        "tags": [],
+        "url": "/pages/Simple",
+        "teaser": null
+      },{
+        "title": "Posts by Category",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "/categories/",
+        "teaser": null
+      },{
+        "title": "Posts by Tag",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "/tags/",
+        "teaser": null
+      },{
+        "title": "Posts by Year",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "/posts/",
+        "teaser": null
+      },{
+        "title": "Welcome to My Smartdown-enabled Blog",
+        "excerpt":"## Welcome to My Smartdown-enabled Blog This is the first post in this example Blog, created with [smartdown/blog-kit](https://github.com/smartdown/blog-kit/). ### Testing Liquid-immunity via `raw` - Test of `{{ site.baseurl }}`: {{ site.baseurl }} - Test of `{{ page.path }}`: {{ page.path }} ### Tunnel Links [Smartdown Tour](:@/posts/ABriefTourOfSmartdown.md) [Welcome To My Blog](:@/posts/WelcomeToMyBlog.md)...","categories": ["smartdown"],
+        "tags": ["welcome","smartdown"],
+        "url": "/posts/WelcomeToMyBlog",
+        "teaser": null
+      },{
+        "title": "Plain Old Markdown",
+        "excerpt":"## GitHub-Flavored Markdown with Kramdown BlogKit supports the standard [GitHub-flavored Markdown](https://help.github.com/articles/about-writing-and-formatting-on-github/) provided by GitHub Pages. Specifically, GitHub Pages uses the [kramdown](https://kramdown.gettalong.org/quickref.html) processor to convert Markdown into HTML within the GitHub Pages server. ### Markdown Examples Here are some simple examples of Markdown. #### Styles It's very easy to make some...","categories": ["markdown"],
+        "tags": ["markdown"],
+        "url": "/posts/PlainOldMarkdown",
+        "teaser": null
+      },{
+        "title": "A Brief Tour of Smartdown",
+        "excerpt":"# WelcomeToSmartdown --- ## Welcome to Smartdown Smartdown is an extension of Markdown that includes several additions: - Technical notation support, including [MathJax](https://www.mathjax.org) LaTeX and AsciiMath syntax, chemical notation via [`mhchem`](https://mhchem.github.io/MathJax-mhchem/), - Syntax highlighting via [highlight.js](https://highlightjs.org). - Reactive cells similar to those in a spreadsheet, but inline with Smartdown prose....","categories": ["smartdown"],
+        "tags": ["welcome","smartdown"],
+        "url": "/posts/ABriefTourOfSmartdown",
+        "teaser": null
+      }]
